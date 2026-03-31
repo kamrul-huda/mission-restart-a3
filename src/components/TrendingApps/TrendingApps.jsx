247e871b300f1188b3c8c6c52e1a4bddccf647a4
Md. Kamrul Huda - BGIT / ACC <kamrul.huda@bgc-bd.com>
2026-03-31 22:31:52 +0600

    minore changes

--- src/components/TrendingApps/TrendingApps.jsx
+++ src/components/TrendingApps/TrendingApps.jsx
@@ -3,7 +3,7 @@ import App from "../App/App";
 
 const TrendingApps = ({ apps }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
       {apps.slice(0, 8).map((app) => (
         <App key={app.id} app={app}></App>
       ))}
