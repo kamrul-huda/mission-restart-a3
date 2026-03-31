ada7e9540264c8e7ba2093cf2707aef297eefeae
kamrul-huda <kamrul.huda@gmail.com>
2026-03-31 15:50:34 +0600

    page navigation loader

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "./components/Shared/Footer";
 import Navbar from "./components/Shared/Navbar";
 
 const Layout = () => {
+  const navigation = useNavigation();
   return (
     <div className="">
+      {navigation.state === "loading" && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/20 z-50">
+          <span className="loading loading-spinner loading-lg text-primary"></span>
+        </div>
+      )}
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
